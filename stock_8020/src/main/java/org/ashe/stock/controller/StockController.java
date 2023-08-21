c7ccc3bb4ed6109492fba911fd228c61087a19de
A <ashet@qq.com>
2023-08-22 00:04:45 +0800

    initial commit

---
+++ stock_8020/src/main/java/org/ashe/stock/controller/StockController.java
@@ -0,0 +1,16 @@
+package org.ashe.stock.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stock")
+public class StockController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+}
